960520510bb8150c114bf415d9b4de94113e6dce
James Cherti <60946298+jamescherti@users.noreply.github.com>
2024-11-07 23:26:48 -0500

    Enhance compile-angel.el and update README.md

--- README.md
+++ README.md
@@ -3,11 +3,15 @@
 ![License](https://img.shields.io/github/license/jamescherti/compile-angel.el)
 ![](https://raw.githubusercontent.com/jamescherti/compile-angel.el/main/.images/made-for-gnu-emacs.svg)
 
-Compile Emacs Lisp libraries automatically. 
+The **compile-angel** package automatically byte-compiles and native-compiles Emacs Lisp libraries. It offers two modes:
+- `(compile-angel-on-save-mode)`: Compiles when an .el file is modified and saved.
+- `(compile-angel-on-load-mode)`: Compiles the .el file before it is loaded.
 
-This is a lightweight alternative to `auto-compile`. Here are the main differences with `auto-compile`:
-- compile-angel does not only compile Emacs Lisp files loaded with `load` and `require`, but also handles files that `auto-compile` misses, such as those that are deferred (e.g., with `defer t` and `use-package`) or `autoload`.
-- `compile-angel` is lightweight (200 lines vs 885 lines for `auto-compile`)
+This package is an alternative to auto-compile. Here are the main differences:
+- The compile-angel package is lightweight, with one-third the lines of code of auto-compile.
+- The auto-compile package handles compiling the elisp files that are loaded using `load` and `require` only.  The compile-angel package, in addition to load and require, also handles files that auto-compile misses, such as those that are deferred (e.g., with `:defer t` and `use-package`) or `autoload`.
+
+(Special thanks to Jonas Bernoulli, the creator of the auto-compile package, whose work inspired the development of compile-angel. This package was created to offer a lightweight alternative to auto-compile that misses fewer deferred/autoloaded files.)
 
 ## Installation
 
@@ -23,7 +27,10 @@ To install `compile-angel` using `straight.el`:
   :straight (compile-angel
              :type git
              :host github
-             :repo "jamescherti/compile-angel.el"))
+             :repo "jamescherti/compile-angel.el")
+  :config
+  (compile-angel-on-save-mode)
+  (compile-angel-on-load-mode))
 ```
 
 ## Author and License
@@ -37,3 +44,15 @@ This program is free software: you can redistribute it and/or modify it under th
 ## Links
 
 - [compile-angel.el @GitHub](https://github.com/jamescherti/compile-angel.el)
+
+Other Emacs packages by the same author:
+- [minimal-emacs.d](https://github.com/jamescherti/minimal-emacs.d): This repository hosts a minimal Emacs configuration designed to serve as a foundation for your vanilla Emacs setup and provide a solid base for an enhanced Emacs experience.
+- [outline-indent.el](https://github.com/jamescherti/outline-indent.el): An Emacs package that provides a minor mode that enables code folding and outlining based on indentation levels for various indentation-based text files, such as YAML, Python, and other indented text files.
+- [vim-tab-bar.el](https://github.com/jamescherti/vim-tab-bar.el): Make the Emacs tab-bar Look Like Vim’s Tab Bar.
+- [easysession.el](https://github.com/jamescherti/easysession.el): Easysession is lightweight Emacs session manager that can persist and restore file editing buffers, indirect buffers/clones, Dired buffers, the tab-bar, and the Emacs frames (with or without the Emacs frames size, width, and height).
+- [elispcomp](https://github.com/jamescherti/elispcomp): A command line tool that allows compiling Elisp code directly from the terminal or from a shell script. It facilitates the generation of optimized .elc (byte-compiled) and .eln (native-compiled) files.
+- [tomorrow-night-deepblue-theme.el](https://github.com/jamescherti/tomorrow-night-deepblue-theme.el): The Tomorrow Night Deepblue Emacs theme is a beautiful deep blue variant of the Tomorrow Night theme, which is renowned for its elegant color palette that is pleasing to the eyes. It features a deep blue background color that creates a calming atmosphere. The theme is also a great choice for those who miss the blue themes that were trendy a few years ago.
+- [Ultyas](https://github.com/jamescherti/ultyas/): A command-line tool designed to simplify the process of converting code snippets from UltiSnips to YASnippet format.
+- [dir-config.el](https://github.com/jamescherti/dir-config.el): Automatically find and evaluate .dir-config.el Elisp files to configure directory-specific settings.
+- [flymake-bashate.el](https://github.com/jamescherti/flymake-bashate.el): A package that provides a Flymake backend for the bashate Bash script style checker.
+- [flymake-ansible-lint.el](https://github.com/jamescherti/flymake-ansible-lint.el): An Emacs package that offers a Flymake backend for `ansible-lint`.
--- compile-angel.el
+++ compile-angel.el
@@ -6,7 +6,7 @@
 ;; Version: 0.9.9
 ;; URL: https://github.com/jamescherti/compile-angel.el
 ;; Keywords: convenience
-;; Package-Requires: ((emacs "24.1"))
+;; Package-Requires: ((emacs "24.4"))
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
 ;; This file is free software; you can redistribute it and/or modify
@@ -27,33 +27,214 @@
 
 ;;; Code:
 
+;;; Variables
+
 (defgroup compile-angel nil
-  "Non-nil if compile-angel mode mode is enabled."
+  "Compile Emacs Lisp libraries automatically."
   :group 'compile-angel
-  :prefix "compile-angel-"
-  :link '(url-link
-          :tag "Github"
-          "https://github.com/jamescherti/compile-angel.el"))
+  :prefix "compile-angel-")
 
-(defun compile-angel--message (&rest args)
-  "Display a message with '[compile-angel]' prepended.
-The message is formatted with the provided arguments ARGS."
-  (apply #'message (concat "[compile-angel] " (car args)) (cdr args)))
+(defcustom compile-angel-display-buffer nil
+  "Non-nil to automatically display the *Compile-Log* buffer."
+  :type 'boolean
+  :group 'compile-angel)
 
-(defun compile-angel--warning (&rest args)
-  "Display a warning message with '[compile-angel] Warning: ' prepended.
-The message is formatted with the provided arguments ARGS."
-  (apply #'message (concat "[compile-angel] Warning: " (car args)) (cdr args)))
+(defcustom compile-angel-verbose nil
+  "Non-nil to display more messages."
+  :type 'boolean
+  :group 'compile-angel)
+
+;;; Internal variables
+
+(defvar compile-angel--list-compiled-files nil)
+(defvar compile-angel--status-byte-compile-skip nil)
+(defvar warning-minimum-level)
+
+;;; Functions
+
+(defun compile-angel--file-ends-with-load-file-suffix (filename base-extension)
+  "Return the FILENAME minus suffix from `load-file-rep-suffixes` if it matches.
+BASE-EXTENSION is the extension before one of the `load-file-rep-suffixes`.
+For example, .el in the case of .el and .el.gz files."
+  (let ((matched nil)
+        (stripped-filename filename))
+    (dolist (suffix load-file-rep-suffixes)
+      (let ((full-suffix (concat base-extension suffix)))
+        (when (string-suffix-p full-suffix filename)
+          (setq stripped-filename
+                (substring filename 0 (- (length filename) (length suffix))))
+          (setq matched t))))
+    (if (and matched (string-suffix-p base-extension stripped-filename))
+        stripped-filename
+      nil)))
+
+(defun compile-angel--native-compile (el-file)
+  "Native compile EL-FILE."
+  (when (and (featurep 'native-compile)
+             (fboundp 'native-comp-available-p)
+             (native-comp-available-p))
+    (if (file-exists-p el-file)
+        (let ((warning-minimum-level (if compile-angel-display-buffer
+                                         :warning
+                                       :error))
+              (eln-file (comp-el-to-eln-filename el-file)))
+          (when (or (not (file-exists-p eln-file))
+                    (file-newer-than-file-p el-file eln-file))
+            (when compile-angel-verbose
+              (message "[compile-angel] Native compile: %s" el-file))
+            (native-compile-async el-file)))
+      (message "[compile-angel] The file does not exist: %s" el-file))))
+
+(defun compile-angel--byte-compile (el-file elc-file)
+  "Byte compile EL-FILE into ELC-FILE."
+  (unless (member el-file compile-angel--status-byte-compile-skip)
+    (if (not (file-writable-p elc-file))
+        (when compile-angel-verbose
+          (message "[compile-angel] Byte compile ignored (not writable): %s"
+                   elc-file)
+          nil)
+      ;; Byte-compile
+      (let ((byte-compile-verbose compile-angel-verbose)
+            (warning-minimum-level (if compile-angel-display-buffer
+                                       :warning
+                                     :error))
+            (byte-compile-result
+             (let ((after-change-major-mode-hook
+                    (and (fboundp 'global-font-lock-mode-enable-in-buffer)
+                         (list 'global-font-lock-mode-enable-in-buffer)))
+                   (inhibit-message (not compile-angel-verbose))
+                   (prog-mode-hook nil)
+                   (emacs-lisp-mode-hook nil))
+               (byte-compile-file el-file))))
+        (cond
+         ;; Ignore (no-byte-compile)
+         ((eq byte-compile-result 'no-byte-compile)
+          (when compile-angel-verbose
+            (message "[compile-angel] Ignore (no-byte-compile): %s"
+                     el-file))
+          (push el-file compile-angel--status-byte-compile-skip)
+          ;; Return nil
+          nil)
+
+         ;; Ignore: Byte compilation error
+         ((not byte-compile-result)
+          (when compile-angel-verbose
+            (message "[compile-angel] Compilation error: %s" el-file))
+          (push el-file compile-angel--status-byte-compile-skip)
+          ;; Return nil
+          nil)
+
+         ;; Success
+         (byte-compile-result
+          (progn
+            (when compile-angel-verbose
+              (message "[compile-angel] Compile: %s" el-file))
+            ;; Compiled. Return t
+            t)))))))
+
+(defun compile-angel-compile-elisp (el-file)
+  "Byte compile and Native compile the .el file EL-FILE."
+  (interactive)
+  (let* ((el-file-sans-suffix (compile-angel--file-ends-with-load-file-suffix
+                               el-file ".el")))
+    (cond
+     ((not (file-exists-p el-file))
+      (message "[compile-angel] Warning: The file does not exist: %s"
+               el-file))
+
+     ((not el-file-sans-suffix)
+      (message "[compile-angel] Warning: The file is not an .el file: %s"
+               el-file))
+
+     (t
+      ;; 1. Byte compile
+      (unwind-protect
+          (let* ((elc-file (concat el-file-sans-suffix "c"))
+                 (elc-file-exists (file-exists-p elc-file)))
+            (when (or (not elc-file-exists)
+                      (file-newer-than-file-p el-file elc-file))
+              (compile-angel--byte-compile el-file
+                                           elc-file)))
+        ;; 2. Always native compile
+        (compile-angel--native-compile el-file))))))
+
+(defun compile-angel--compile-current-buffer ()
+  "Compile the current buffer."
+  (when (derived-mode-p 'emacs-lisp-mode)
+    (let ((el-file (buffer-file-name (buffer-base-buffer))))
+      (compile-angel-compile-elisp el-file))))
+
+(defun compile-angel--locate-library (library nosuffix)
+  "Return the path to the LIBRARY el file.
+Use `load-file-rep-suffixes' when NOSUFFIX is non-nil."
+  (locate-file (substitute-in-file-name library)
+               load-path
+               (if nosuffix
+                   load-file-rep-suffixes
+                 (mapcar (lambda (s) (concat ".el" s))
+                         load-file-rep-suffixes))))
+
+(defun compile-angel--advice-before-require (feature
+                                             &optional filename _noerror)
+  "Recompile the library before `require'.
+FEATURE and FILENAME are the same arguments as the `require' function."
+  (when (or feature filename)
+    (let* ((el-file (compile-angel--locate-library
+                     (or filename (symbol-name feature))
+                     nil)))
+      (when el-file
+        (when (not (member el-file compile-angel--list-compiled-files))
+          (push el-file compile-angel--list-compiled-files)
+          (compile-angel-compile-elisp el-file))))))
+
+(defun compile-angel--advice-before-load (el-file &optional _noerror
+                                                  _nomessage nosuffix
+                                                  _must-suffix)
+  "Recompile before `load'.
+EL-FILE and NOSUFFIX are the same arguments as `load'."
+  (let ((el-file (compile-angel--locate-library el-file
+                                                nosuffix)))
+    (when el-file
+      (when (not (member el-file compile-angel--list-compiled-files))
+        (push el-file compile-angel--list-compiled-files)
+        (compile-angel-compile-elisp el-file)))))
+
+(defun compile-angel--advice-before-autoload (_function
+                                              file-or-feature
+                                              &optional
+                                              _docstring _interactive _type)
+  "Recompile before `autoload'.
+FILE-OR-FEATURE is the file or the feature."
+  (let ((el-file (compile-angel--locate-library file-or-feature nil)))
+    (when (and el-file (not (member el-file
+                                    compile-angel--list-compiled-files)))
+      (push el-file compile-angel--list-compiled-files)
+      (compile-angel-compile-elisp el-file))))
 
 ;;;###autoload
-(define-minor-mode compile-angel-mode
+(define-minor-mode compile-angel-on-load-mode
   "Toggle `compile-angel-mode'."
   :global t
-  :lighter " compile-angel"
+  :lighter " CompAngelLd"
   :group 'compile-angel
-  (if compile-angel-mode
-      t
-    t))
+  (if compile-angel-on-load-mode
+      (progn
+        (advice-add 'autoload :before #'compile-angel--advice-before-autoload)
+        (advice-add 'require :before #'compile-angel--advice-before-require)
+        (advice-add 'load :before #'compile-angel--advice-before-load))
+    (advice-remove 'autoload #'compile-angel--advice-before-autoload)
+    (advice-remove 'require 'compile-angel--advice-before-require)
+    (advice-remove 'load 'compile-angel--advice-before-load)))
+
+;;;###autoload
+(define-minor-mode compile-angel-on-save-mode
+  "Toggle `compile-angel-mode'."
+  :global t
+  :lighter " CompAngelSv"
+  :group 'compile-angel
+  (if compile-angel-on-save-mode
+      (add-hook 'after-save-hook #'compile-angel--compile-current-buffer)
+    (remove-hook 'after-save-hook #'compile-angel--compile-current-buffer)))
 
 (provide 'compile-angel)
 ;;; compile-angel.el ends here
