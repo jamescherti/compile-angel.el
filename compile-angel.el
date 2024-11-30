67a12504ebf2cc43a6846a3e05b6e5c2e179db2f
James Cherti <60946298+jamescherti@users.noreply.github.com>
2024-11-29 23:42:07 -0500

    Release: 1.0.4

--- compile-angel.el
+++ compile-angel.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2024 James Cherti | https://www.jamescherti.com/contact/
 
 ;; Author: James Cherti
-;; Version: 1.0.3
+;; Version: 1.0.4
 ;; URL: https://github.com/jamescherti/compile-angel.el
 ;; Keywords: convenience
 ;; Package-Requires: ((emacs "26.1"))
