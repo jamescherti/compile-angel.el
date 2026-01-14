59b431c6a55b8aeb36f536756890adc3b70ee205
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-01-14 10:35:19 -0500

    Update copyright year

--- compile-angel.el
+++ compile-angel.el
@@ -1,6 +1,6 @@
 ;;; compile-angel.el --- Automatically Compile Elisp files (auto-compile alternative) -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2024-2025 James Cherti | https://www.jamescherti.com/contact/
+;; Copyright (C) 2024-2026 James Cherti | https://www.jamescherti.com/contact/
 
 ;; Author: James Cherti
 ;; Version: 1.1.3
--- tests/test-compile-angel.el
+++ tests/test-compile-angel.el
@@ -1,6 +1,6 @@
 ;;; test-compile-angel.el --- Test compile-angel -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2025 James Cherti | https://www.jamescherti.com/contact/
+;; Copyright (C) 2024-2026 James Cherti | https://www.jamescherti.com/contact/
 
 ;; Author: James Cherti
 ;; Version: 1.1.3
