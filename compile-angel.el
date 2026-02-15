967500e480cf6ee34bc1389e9df6ee0fe8fd53e3
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-02-15 15:13:14 -0500

    Update header

--- compile-angel.el
+++ compile-angel.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2024-2026 James Cherti | https://www.jamescherti.com/contact/
 
-;; Author: James Cherti
+;; Author: James Cherti <https://www.jamescherti.com/contact/>
 ;; Version: 1.1.3
 ;; URL: https://github.com/jamescherti/compile-angel.el
 ;; Keywords: convenience
